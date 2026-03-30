97cb7c6a2237c9e38a6a6c1b06ca65f4249c5c0a
Johanna <majliiz@hotmail.com>
2026-03-30 14:41:32 +0200

    add redirect to docs

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from app.routers import recipes
 
 app = FastAPI()
+
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")
+
 
 # Register routers
 app.include_router(recipes.router)
